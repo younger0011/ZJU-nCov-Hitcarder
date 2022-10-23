24fbddd398b15f72b90c7d6de4240aa1641c54c2
Lokilankaaa <xw7771428@gmail.com>
2022-10-23 14:48:33 +0800

    Update hitcarder.py

--- hitcarder.py
+++ hitcarder.py
@@ -195,7 +195,7 @@ def main(username, password):
     try:
         ret = hit_carder.check_form()
         if not ret:
-            return 2, '打卡信息已改变，请手动打卡'
+            return 2, '打卡信息已改变，请手动打卡___'
     except Exception as err:
         return 1, '获取信息失败，请手动打卡: ' + str(err)
 
